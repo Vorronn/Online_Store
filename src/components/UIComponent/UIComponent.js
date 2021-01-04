5461a7735b6462447be2bc6905b527ffc9875a9a
Siarhei Sapelka <Sereban778@yandex.ru>
2021-01-04 18:36:56 +0300

    add html and css for footer

--- src/components/UIComponent/UIComponent.js
+++ src/components/UIComponent/UIComponent.js
@@ -5,6 +5,7 @@ import basket from "../../assets/images/basket.png";
 import favorite from "../../assets/images/favorite.png";
 import user from "../../assets/images/user.png";
 import search from "../../assets/images/search.png"
+import logo from "../../assets/images/logo_2.png";
 
 const UIComponent = (props) => {
     return (
@@ -22,7 +23,7 @@ const UIComponent = (props) => {
                     </div>
                 </div>
                 <div className={styles.headerLogo}>
-                    <img src={"https://static.rfstat.com/logo-presets/103/thumbnail_b3851c2e1943_1x.jpeg"} />
+                    <img src={logo} />
                 </div>
                 <nav className={styles.headerLinkMenu}>
                     <NavLink to={"main"} activeClassName={styles.activeLink}>Main</NavLink>
@@ -51,8 +52,43 @@ const UIComponent = (props) => {
                     <NavLink className={styles.endLink +" "+ styles.navProduct} to={"power-bank"} activeClassName={styles.activeNavProducts}>Power bank</NavLink>
                 </nav>
             </header>
-            <main className={styles.main}>Main</main>
-            <footer className={styles.footer}>Footer</footer>
+            <main className={styles.main}>
+                <div className={styles.mainSection}>
+                    <section>
+                        <img src={logo}/>
+                    </section>
+                </div>
+            </main>
+            <footer className={styles.footer}>
+                <div className={styles.footerLogo}>
+                    <img src={logo} />
+                </div>
+                <div className={styles.footerBlock}>
+                    <h3>Online store</h3>
+                    <NavLink to={"main"} activeClassName={styles.activeLinkFooter}>Main</NavLink>
+                    <NavLink to={"payment"} activeClassName={styles.activeLinkFooter}>Payment</NavLink>
+                    <NavLink to={"delivery"} activeClassName={styles.activeLinkFooter}>Delivery</NavLink>
+                    <NavLink to={"bonuses"} activeClassName={styles.activeLinkFooter}>Bonuses</NavLink>
+                    <NavLink to={"contacts"} activeClassName={styles.activeLinkFooter}>Contacts</NavLink>
+                </div>
+                <div className={styles.footerBlock}>
+                    <h3>Products</h3>
+                    <NavLink to={"mobile-phone"} activeClassName={styles.activeLinkFooter}>Mobile phones</NavLink>
+                    <NavLink to={"headphones"} activeClassName={styles.activeLinkFooter}>Headphones</NavLink>
+                    <NavLink to={"phone-cases"} activeClassName={styles.activeLinkFooter}>Phone cases</NavLink>
+                    <NavLink to={"battery-charger"} activeClassName={styles.activeLinkFooter}>Battery charger</NavLink>
+                    <NavLink to={"power-bank"} activeClassName={styles.activeLinkFooter}>Power bank</NavLink>
+                </div>
+                <div className={styles.footerBlock}>
+                    <h3>Account</h3>
+                    <NavLink to={"user"} activeClassName={styles.activeLinkFooter}>User</NavLink>
+                    <NavLink to={"favorite"} activeClassName={styles.activeLinkFooter}>Favorite</NavLink>
+                    <NavLink to={"basket"} activeClassName={styles.activeLinkFooter}>Basket</NavLink>
+                </div>
+                <div className={styles.footerCopy}>
+                    <h3>Copy, 2021</h3>
+                </div>
+            </footer>
         </div>
 
     )
