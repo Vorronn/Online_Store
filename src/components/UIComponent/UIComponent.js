4901d6092f1abf5cd912c600f86dc5e01aa00e8c
Siarhei Sapelka <Sereban778@yandex.ru>
2021-01-05 03:33:29 +0300

    add html and css for main section

--- src/components/UIComponent/UIComponent.js
+++ src/components/UIComponent/UIComponent.js
@@ -6,6 +6,8 @@ import favorite from "../../assets/images/favorite.png";
 import user from "../../assets/images/user.png";
 import search from "../../assets/images/search.png"
 import logo from "../../assets/images/logo_2.png";
+import android from "../../assets/images/android.png";
+import iphone from "../../assets/images/iphone.png";
 
 const UIComponent = (props) => {
     return (
@@ -23,7 +25,7 @@ const UIComponent = (props) => {
                     </div>
                 </div>
                 <div className={styles.headerLogo}>
-                    <img src={logo} />
+                    <img src={logo} alt={"logo"}/>
                 </div>
                 <nav className={styles.headerLinkMenu}>
                     <NavLink to={"main"} activeClassName={styles.activeLink}>Main</NavLink>
@@ -35,14 +37,14 @@ const UIComponent = (props) => {
                 <div className={styles.headerSearch}>
                     <div className={styles.inputForm}>
                         <input className={styles.inputSearch} type={"search"} placeholder={"Search"} />
-                        <button className={styles.inputButton} onClick={()=>(alert("hello"))}><img src={search}/></button>
+                        <button className={styles.inputButton} onClick={()=>(alert("hello"))}><img src={search} alt={"searchButton"}/></button>
                     </div>
 
                 </div>
                 <nav className={styles.headerNavAccount}>
-                    <NavLink to={"user"} activeClassName={styles.activeNavAccount}><img src={user}/></NavLink>
-                    <NavLink to={"favorite"} activeClassName={styles.activeNavAccount}><img src={favorite}/></NavLink>
-                    <NavLink to={"basket"} activeClassName={styles.activeNavAccount}><img src={basket}/></NavLink>
+                    <NavLink to={"user"} activeClassName={styles.activeNavAccount}><img src={user} alt={"userAccount"}/></NavLink>
+                    <NavLink to={"favorite"} activeClassName={styles.activeNavAccount}><img src={favorite} alt={"favorite"}/></NavLink>
+                    <NavLink to={"basket"} activeClassName={styles.activeNavAccount}><img src={basket} alt={"basket"}/></NavLink>
                 </nav>
                 <nav className={styles.headerNavProducts}>
                     <NavLink className={styles.startLink +" "+ styles.navProduct} to={"mobile-phone"} activeClassName={styles.activeNavProducts}>Mobile phones</NavLink>
@@ -54,14 +56,147 @@ const UIComponent = (props) => {
             </header>
             <main className={styles.main}>
                 <div className={styles.mainSection}>
-                    <section>
-                        <img src={logo}/>
+                    <h2 className={styles.productsTitle}>Mobile Phone</h2>
+                    <div className={styles.selectFilter}>
+                        <div className={styles.navigation}>
+                            <span>1</span>
+                            <span>2</span>
+                            <span>3</span>
+                            <span>4</span>
+                            <span>5</span>
+                        </div>
+                        <div className={styles.countProducts}>
+                            <span>the number on the page </span>
+                            <select>
+                                <option>10</option>
+                                <option>20</option>
+                                <option>50</option>
+                            </select>
+                        </div>
+                        <div className={styles.sortingProducts}>
+                            <span>filter </span>
+                            <select>
+                                <option>by popularity</option>
+                                <option>price in ascending order</option>
+                                <option>price in descending order</option>
+                            </select>
+                        </div>
+                    </div>
+                    <section className={styles.products}>
+                        <div className={styles.productItems}>
+                            <div className={styles.productItem}>
+                                <h3 className={styles.productName}>Samsung</h3>
+                                <div className={styles.productImage}>
+                                    <img src={android} alt={"mobile Phone"} />
+                                </div>
+                                <div className={styles.productDescription}>This description product This description product This description product This description product</div>
+                                <div className={styles.productButtonGroup}>
+                                    <button className={styles.productButtonBasket}>In basket</button>
+                                    <button className={styles.productButtonFavorite}>In favorite</button>
+                                </div>
+                            </div>
+                            <div className={styles.productItem}>
+                                <h3 className={styles.productName}>Samsung</h3>
+                                <div className={styles.productImage}>
+                                    <img src={iphone} alt={"mobile Phone"} />
+                                </div>
+                                <div className={styles.productDescription}>This description product</div>
+                                <div className={styles.productButtonGroup}>
+                                    <button className={styles.productButtonBasket}>In basket</button>
+                                    <button className={styles.productButtonFavorite}>In favorite</button>
+                                </div>
+                            </div>
+                            <div className={styles.productItem}>
+                                <h3 className={styles.productName}>Samsung</h3>
+                                <div className={styles.productImage}>
+                                    <img src={android} alt={"mobile Phone"} />
+                                </div>
+                                <div className={styles.productDescription}>This description product</div>
+                                <div className={styles.productButtonGroup}>
+                                    <button className={styles.productButtonBasket}>In basket</button>
+                                    <button className={styles.productButtonFavorite}>In favorite</button>
+                                </div>
+                            </div>
+                            <div className={styles.productItem}>
+                                <h3 className={styles.productName}>Samsung</h3>
+                                <div className={styles.productImage}>
+                                    <img src={android} alt={"mobile Phone"} />
+                                </div>
+                                <div className={styles.productDescription}>This description product</div>
+                                <div className={styles.productButtonGroup}>
+                                    <button className={styles.productButtonBasket}>In basket</button>
+                                    <button className={styles.productButtonFavorite}>In favorite</button>
+                                </div>
+                            </div>
+                            <div className={styles.productItem}>
+                                <h3 className={styles.productName}>Samsung</h3>
+                                <div className={styles.productImage}>
+                                    <img src={android} alt={"mobile Phone"} />
+                                </div>
+                                <div className={styles.productDescription}>This description product</div>
+                                <div className={styles.productButtonGroup}>
+                                    <button className={styles.productButtonBasket}>In basket</button>
+                                    <button className={styles.productButtonFavorite}>In favorite</button>
+                                </div>
+                            </div>
+                        </div>
                     </section>
+                    <section className={styles.filter}>
+                        <div className={styles.price}>
+                            <h4 className={styles.priceName}>Price</h4>
+                            <input type={"text"} placeholder={"min price"}/>
+                            <input type={"text"} placeholder={"max price"}/>
+                        </div>
+                        <div className={styles.brand}>
+                            <h4 className={styles.brandName}>Brand</h4>
+                            <div className={styles.brandItem}>
+                                <input type={"checkbox"} /> Samsung
+                            </div>
+                            <div className={styles.brandItem}>
+                                <input type={"checkbox"} /> Iphone
+                            </div>
+                            <div className={styles.brandItem}>
+                                <input type={"checkbox"} /> Honor
+                            </div>
+                            <div className={styles.brandItem}>
+                                <input type={"checkbox"} /> ZTE
+                            </div>
+                            <div className={styles.brandItem}>
+                                <input type={"checkbox"} /> Xiaomi
+                            </div>
+                        </div>
+                        <button className={styles.filterSubmit}>Submit</button>
+                    </section>
+                    <div className={styles.selectFilterBottom}>
+                        <div className={styles.navigation}>
+                            <span>1</span>
+                            <span>2</span>
+                            <span>3</span>
+                            <span>4</span>
+                            <span>5</span>
+                        </div>
+                        <div className={styles.countProducts}>
+                            <span>the number on the page </span>
+                            <select>
+                                <option>10</option>
+                                <option>20</option>
+                                <option>50</option>
+                            </select>
+                        </div>
+                        <div className={styles.sortingProducts}>
+                            <span>filter </span>
+                            <select>
+                                <option>by popularity</option>
+                                <option>price in ascending order</option>
+                                <option>price in descending order</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </main>
             <footer className={styles.footer}>
                 <div className={styles.footerLogo}>
-                    <img src={logo} />
+                    <img src={logo} alt={"logo"}/>
                 </div>
                 <div className={styles.footerBlock}>
                     <h3>Online store</h3>
